8a0b959af9e7c0400bebe590b017ad718d1f617d
sofi fernandes <sofi-fernandes@live.com>
2020-09-02 18:46:16 -0300

    versao 1

---
+++ ProjetoIntegrador/HoldingHands/src/main/java/com/etcetera/HoldingHands/HoldingHandsApplication.java
@@ -0,0 +1,13 @@
+package com.etcetera.HoldingHands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoldingHandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoldingHandsApplication.class, args);
+	}
+
+}
---
+++ ProjetoIntegrador/HoldingHands/src/test/java/com/etcetera/HoldingHands/HoldingHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etcetera.HoldingHands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoldingHandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
